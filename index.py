bd26ae53cf15ac40b9458931733e99e4f14a0b6a
MdMehedi-Hasan <mh19115@gmail.com>
2025-09-06 16:14:30 +0600

    first commit

---
+++ index.py
@@ -0,0 +1,27 @@
+# conditional statements
+x = 12
+y = 20
+
+if x < y:
+    print("y is greater than x")
+else:
+    print("x is greater than y")
+
+# Loop (for)
+
+l1 = ["dsfs","dfgdfg","dfgdfg"]
+for x in l1:
+    print(x)
+
+# Loop (while)
+
+i = 5
+while i<20:
+    print(i)
+    i = i+1
+
+# function
+
+def first_Function(num):
+    print("inside function",num)
+first_Function(5)
